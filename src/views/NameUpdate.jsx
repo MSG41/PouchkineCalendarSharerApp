5d39124328c531b49d8767b416055d4bc7fa0579
MSG41 <s.thaifa@amsterdam.nl>
2023-02-03 22:49:25 +0100

    yess, the form is working and is updated git status! top top ! nice first step

--- src/views/NameUpdate.jsx
+++ src/views/NameUpdate.jsx
@@ -1,32 +1,152 @@
+import { useState } from "react";
+import "./NameUpdate.css";
+
 // import { createClient } from "contentful-management";
 const contentful = require("contentful-management");
 
 const client = contentful.createClient({
   accessToken: "CFPAT-HPgZzJrSBWw7tZvKo099X0Ua7d3y7XzAD9Ugk8cvceM",
 });
+
 const NameUpdate = () => {
-  client
-    .getSpace("3seggq75gekz")
-    .then((space) => space.getEnvironment("master"))
-    .then((environment) =>
-      environment.createEntry("form", {
-        fields: {
-          eventTitle: {
-            "en-US": "This is a new entry - yayyyy it works super nicely !!!! ",
-          },
-        },
-      })
-    )
-    .then((entry) => console.log(entry))
-    .catch(console.error);
+  const [formData, setFormData] = useState({
+    eventTitle: "",
+    eventDescription: "",
+  });
+  const [error, setError] = useState({ eventTitle: "", eventDescription: "" });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    if (value.length <= 0 || value.length < 3 || value.length > 60) {
+      setError({
+        ...error,
+        [name]: "Event title must be between 5 and 60 characters",
+      });
+    } else {
+      setError({ ...error, [name]: "" });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!error.eventTitle && !error.eventDescription) {
+      // submit form data
+      alert("The form is submitted!!!!!! Bravo!!!", !error);
+    } else alert("Event title must be between 5 and 60 characters", error);
+
+    try {
+      const response = await client
+        .getSpace("3seggq75gekz")
+        .then((space) => space.getEnvironment("master"))
+        .then((environment) =>
+          environment.createEntry("form", {
+            fields: {
+              eventTitle: {
+                "en-US": formData.eventTitle,
+              },
+              eventDescription: {
+                "en-US": formData.eventDescription,
+              },
+            },
+          })
+        )
+        .then((entry) => console.log(entry))
+        .catch(console.error);
+      console.log("the Title is submitted!!!!!!!! bravo", response);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
-    <form>
-      <input type="text" name="field1" />
-      <input type="text" name="field2" />
-      <button type="submit">Submit</button>
+    <form className="form" onSubmit={handleSubmit}>
+      <label className="label">
+        Event Title
+        <input
+          type="text"
+          name="eventTitle"
+          value={formData.eventTitle}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label className="label">
+        Event Description
+        <input
+          type="text"
+          name="eventDescription"
+          value={formData.eventDescription}
+          onChange={handleChange}
+        />
+      </label>
+
+      {(formData.eventTitle.length >= 3 && formData.eventDescription.length) >=
+      3 ? (
+        <button className="button" type="submit">
+          Submit
+        </button>
+      ) : null}
+      {/* {!error.eventTitle && !error.eventDescription ? (
+        <button className="button" type="submit">
+          Submit
+        </button>
+      ) : null} */}
     </form>
   );
+
+  // <form
+  //   style={{
+  //     position: "absolute",
+  //     top: "50vh",
+  //     left: "15%",
+  //   }}
+  //   onSubmit={handleSubmit}
+  // >
+  //   <h1
+  //     style={{
+  //       background: "white",
+  //       borderRadius: "10px",
+  //       width: "fit-content",
+  //       padding: "10px",
+  //       fontSize: "40px",
+  //     }}
+  //   >
+  //     Event Title
+  //   </h1>
+  //   <input
+  //     style={{
+  //       fontSize: "30px",
+  //       padding: "30px",
+  //       marginRight: "20px",
+  //       minWidth: "400px",
+  //       borderRadius: "10px",
+  //     }}
+  //     placeholder="Write your event title here !!!"
+  //     type="text"
+  //     name="eventTitle"
+  //     onChange={handleChange}
+  //   />
+  //   {/* <input type="text" name="eventDescription" /> */}
+  //   <button
+  //     onMouseOver="this.style.color='red'"
+  //     style={{
+  //       background: "white",
+  //       borderRadius: "10px",
+  //       width: "fit-content",
+  //       padding: "10px",
+  //       fontSize: "40px",
+  //     }}
+  //     type="submit"
+  //   >
+  //     Submit
+  //   </button>
+  // </form>
+  // );
 };
 
 export default NameUpdate;
